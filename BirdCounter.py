5adcf971521cda0f9d863ea2f02b1598cec4298f
Dominik Palenik <Dominik.Palenik1@gmail.com>
2023-05-31 17:16:47 +0200

    Improved background in BirdCounter

--- BirdCounter.py
+++ BirdCounter.py
@@ -7,13 +7,11 @@ import Bird
 import GameInfo
 import Screen
 
-
 class BirdCounter:
     IMG = pygame.transform.scale(pygame.image.load("images/gui/bird.png"), (35, 33)).convert_alpha()
     _FONT = pygame.freetype.Font("fonts/timer.ttf", 50)
     color = (255, 255, 255)
-    _FONT_BACKGROUND = pygame.Surface((136, 44))
-    _FONT_BACKGROUND.set_alpha(100)
+    _FONT_BACKGROUND = pygame.Surface((135, 44))
 
     SOUND = pygame.mixer.Sound("sounds/bird.wav")
 
@@ -25,7 +23,7 @@ class BirdCounter:
     def render():
         deltaTime = time.time() - BirdCounter.lastCatchTime
 
-        # font size after 60 seconds
+        # font size after catching bird
         if deltaTime > 0 and deltaTime < 0.3:
             if deltaTime < 0.15:  # font is growing
                 BirdCounter._FONT.size = 50 + deltaTime * 75
@@ -33,7 +31,6 @@ class BirdCounter:
                 BirdCounter._FONT.size = 61.25 - (deltaTime - 0.15) * 75
         else:
             BirdCounter._FONT.size = 50
-
 
         strr = str(BirdCounter.birdsCatched) + "/" + str(BirdCounter.allBirds)
 
@@ -49,13 +46,23 @@ class BirdCounter:
         BirdCounter.allBirds = len(Bird.Bird.allBirds)
         BirdCounter.birdsCatched = 0
         BirdCounter.SOUND.set_volume(GameInfo.GameInfo.getSound())
+        BirdCounter._setFontBackgroundWidth()
 
     @staticmethod
     def catchedBird():
         BirdCounter.birdsCatched += 1
         BirdCounter.lastCatchTime = time.time()
         BirdCounter.SOUND.play()
+        BirdCounter._setFontBackgroundWidth()
 
+    @staticmethod
+    def _setFontBackgroundWidth():
+            stringLen = len(str(BirdCounter.birdsCatched)) + len(str(BirdCounter.allBirds))
+            surfaceWidth = 99 + stringLen * 21
+            BirdCounter._FONT_BACKGROUND = pygame.Surface((surfaceWidth, 44))
+            BirdCounter._FONT_BACKGROUND.set_alpha(100)
+            print(stringLen)
+            print(surfaceWidth)
 
 
 
