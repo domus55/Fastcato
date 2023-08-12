9f1ca17b6eecd56b490603a096a3ddca8f3e57c9
domus55 <dominik.palenik1@gmail.com>
2023-08-12 12:18:58 +0200

    Simplified tutorial

--- HUD/Tutorial.py
+++ HUD/Tutorial.py
@@ -8,11 +8,9 @@ from Screen import screen
 
 
 class Tutorial:
-    leftPanel = pygame.Surface((1200, 900), pygame.SRCALPHA, 32)
     rightPanel = pygame.Surface((400, 900), pygame.SRCALPHA, 32)
 
-    showRight = True
-    showLeft = False
+    showHint = True
 
     @staticmethod
     def render():
@@ -21,27 +19,17 @@ class Tutorial:
             return
 
         if Buttons.Buttons.right or Game.Game.keyPressed[pygame.K_d]:
-            if Tutorial.showRight:
-                Tutorial.showLeft = True
-            Tutorial.showRight = False
-
-        if (Buttons.Buttons.left or Game.Game.keyPressed[pygame.K_a]) and not Tutorial.showRight:
-            Tutorial.showLeft = False
+            Tutorial.showHint = False
 
         # render part
         alpha1 = abs(100 - (Deadline.Deadline.time()) * 25 % 100)
         alpha2 = abs(100 - (Deadline.Deadline.time() + 2) * 25 % 100)
-        if Tutorial.showRight or (Player.Player.getInstance().pos[0] < 900 and Deadline.Deadline.time() >= 12):
+        if Tutorial.showHint or (Player.Player.getInstance().pos[0] < 900 and Deadline.Deadline.time() >= 8):
             if Deadline.Deadline.time() < 4:
                 return
             Tutorial._draw_circle_alpha(Tutorial.rightPanel, (1200, 0), (255, 255, 255, alpha1), (200, 450), 5 * abs(100 - alpha1))
-            if (Tutorial.showRight and Deadline.Deadline.time() > 6) or (not Tutorial.showRight and Deadline.Deadline.time() >= 14 ):
+            if (Tutorial.showHint and Deadline.Deadline.time() > 6) or (not Tutorial.showHint and Deadline.Deadline.time() >= 10):
                 Tutorial._draw_circle_alpha(Tutorial.rightPanel, (1200, 0), (255, 255, 255, alpha2), (200, 450), 5 * abs(100 - alpha2))
-
-        if Tutorial.showLeft:
-            Tutorial._draw_circle_alpha(Tutorial.leftPanel, (0, 0), (255, 255, 255, alpha1), (600, 450), 8 * abs(100 - alpha1))
-            if Deadline.Deadline.time() > 2:
-                Tutorial._draw_circle_alpha(Tutorial.leftPanel, (0, 0), (255, 255, 255, alpha2), (600, 450), 8 * abs(100 - alpha2))
 
     @staticmethod
     def _draw_circle_alpha(panel, panelPos, color, center, radius):
