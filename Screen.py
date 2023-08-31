6f52edbdf04b498c50ff4048ed6b533f2aa70d18
domus55 <dominik.palenik1@gmail.com>
2023-08-31 22:04:28 +0200

    Extended build differences file, deleted unused files and folders

--- Screen.py
+++ Screen.py
@@ -9,7 +9,7 @@ def screenInitialize():
     global screen
     if GameInfo.GameInfo.BUILD_TYPE == GameInfo.BuildType.ANDROID or GameInfo.GameInfo.fullScreen:
         screen = pygame.display.set_mode((1600, 900), pygame.SCALED | pygame.FULLSCREEN)
-    pygame.display.set_caption('FastCato')
+    pygame.display.set_caption('Fastcato')
     icon = pygame.image.load(f"{PATH}images/gameIcon.png")
     pygame.display.set_icon(icon)
 
